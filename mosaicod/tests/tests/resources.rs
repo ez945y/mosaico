9758d0a49f0804a180c6d00d601d1d5c96314865
Federico Cabassi <federico.cabassi@mosaico.dev>
2026-02-26 11:40:23 +0100

    Fixed `sequence_delete` action (#195)
    * Fixed sequence delete delete
    
    - Now sequence delete deletes all the sessions before deleting residual sequence data and
    record. Deletion of topic is done inside the session delete.
    - Added an integration test for sequence_delete
    - Minor refactoring
    
    Relates #192
    
    * Updated .sqlx folder #192
    
    * Resolved typos
    Releates #192
    
    * Fixed compilation error after merge
    #192

--- mosaicod/tests/tests/resources.rs
+++ mosaicod/tests/tests/resources.rs
@@ -156,3 +156,32 @@ async fn session_abort(pool: sqlx::Pool<db::DatabaseType>) {
 
     server.shutdown().await;
 }
+
+#[sqlx::test(migrator = "mosaicod_db::testing::MIGRATOR")]
+async fn sequence_delete(pool: sqlx::Pool<db::DatabaseType>) {
+    let port = common::random_port();
+
+    let server = common::ServerBuilder::new(common::HOST, port, pool)
+        .build()
+        .await;
+
+    let mut client = common::ClientBuilder::new(common::HOST, port).build().await;
+
+    let sequence_name = "test_sequence";
+
+    actions::sequence_create(&mut client, sequence_name, None).await;
+    let session_uuid = actions::session_create(&mut client, sequence_name).await;
+    assert!(session_uuid.is_valid());
+    let uuid =
+        actions::topic_create(&mut client, &session_uuid, "test_sequence/my_topic", None).await;
+    assert!(uuid.is_valid());
+
+    let batches = vec![ext::arrow::testing::dummy_batch()];
+    let _ = actions::do_put(&mut client, &uuid, "test_sequence/my_topic", batches).await;
+
+    actions::session_finalize(&mut client, session_uuid).await;
+
+    actions::sequence_delete(&mut client, "test_sequence").await;
+
+    server.shutdown().await;
+}
